a40dd2f6c72605ad161ddf629324a61480aecb05
RenatoOliveiraS <zyx.renato@gmail.com>
2025-05-09 23:44:53 -0300

    refactor(tests): remove definição do modelo UserModel de test_models.py
    - Elimina a classe UserModel do arquivo de testes, simplificando a estrutura do código.
    - Mantém a importação do modelo User para garantir a continuidade dos testes relacionados.

--- backend/app/tests/test_models.py
+++ backend/app/tests/test_models.py
@@ -1,21 +1,5 @@
 import pytest
 from app.models.user import User
-from app.tests.test_config import Base
-from sqlalchemy import ARRAY, Boolean, Column, DateTime, String
-
-
-class UserModel(Base):
-    __tablename__ = "users"
-
-    id = Column(String, primary_key=True)
-    email = Column(String, unique=True, nullable=False)
-    password_hash = Column(String, nullable=False)
-    name = Column(String, nullable=False)
-    photo = Column(String, nullable=True)
-    roles = Column(ARRAY(String), nullable=False)
-    is_active = Column(Boolean, default=True)
-    created_at = Column(DateTime, nullable=False)
-    updated_at = Column(DateTime, nullable=False)
 
 
 @pytest.mark.asyncio
